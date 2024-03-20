76a0938833dcd24f1ded1a707b42772264ae0911
localuser <localuser@SRR-mal-msigp62.eu.corp.samsungelectronics.net>
2024-03-20 21:10:07 +0300

    Add extended search form mockup

--- ui/ext_search_ui.py
+++ ui/ext_search_ui.py
@@ -1,5 +1,7 @@
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QScrollArea
-
+from PySide6.QtWidgets import (QWidget, QVBoxLayout, QLabel, QScrollArea,
+                               QFormLayout, QLineEdit, QGroupBox, QSizePolicy,
+                               QDateEdit)
+from PySide6.QtCore import QDate
 
 class ExtSearchUI:
 
@@ -9,13 +11,23 @@ class ExtSearchUI:
         layout = QVBoxLayout(widget_container)
         if col == 0:
             layout.addWidget(QLabel('<h2>Search form</h2>'))
+            form_layout = QFormLayout()
+            form_layout.addRow(win.tr("&Description:"), QLineEdit())
+            form_layout.addRow(QLabel(win.tr("Filming date")))
+            form_layout.addRow(win.tr("&From:"), QDateEdit(QDate.currentDate()))
+            form_layout.addRow(win.tr("&To:"), QDateEdit(QDate.currentDate()))
+            form_layout.addRow(QLabel(win.tr("Import date")))
+            form_layout.addRow(win.tr("&From:"), QDateEdit(QDate.currentDate()))
+            form_layout.addRow(win.tr("&To:"), QDateEdit(QDate.currentDate()))
+            groupBox = QGroupBox('')
+            groupBox.setLayout(form_layout)
+            groupBox.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+            layout.addWidget(groupBox)
+
         elif col == 1:
             layout.addWidget(QLabel('<h2>Scenes</h2>'))
         elif col == 2:
             layout.addWidget(QLabel('<h2>Storyboard</h2>'))
 
-        scroll_area = QScrollArea()
-        scroll_area.setWidget(widget_container)
-
         v_main_layout = QVBoxLayout(win)
-        v_main_layout.addWidget(scroll_area)
+        v_main_layout.addWidget(widget_container)
